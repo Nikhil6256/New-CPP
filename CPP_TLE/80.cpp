b5b30f300471ce2bacb1e7c1363b90fefd52095e
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2025-02-07 09:15:10 +0530

    Arrange string

---
+++ CPP_TLE/80.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    string a, b;
+    cin >> a;
+    for (int i = 0; i < a.length(); i += 2)
+    {
+        b += a[i];
+    }
+    sort(b.begin(), b.end());
+    for (int i = 0; i < b.length() - 1; i++)
+        cout << b[i] << "+";
+    cout << b.back() << endl;
+    return 0;
+}
