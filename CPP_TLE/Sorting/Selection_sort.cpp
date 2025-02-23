96d7289423e43a1e30b6483169d824588cc10ed2
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2025-02-23 14:42:52 +0530

    Selection_sort

--- CPP_TLE/Sorting/Selection_sort.cpp
+++ CPP_TLE/Sorting/Selection_sort.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 void Selection_sort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<=n-2;i++){
         int min=i;
-        for(int j=0;j<n-1;j++){
-            if(arr[j]>arr[min]){
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[min]){
                 min=j;
             }
         }
@@ -18,10 +18,12 @@ int main()
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     Selection_sort(arr,n);
-
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
