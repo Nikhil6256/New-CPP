59397b39553bd982d00ef8c013a9b0b9900378a2
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2025-07-30 18:58:11 +0530

    Intro to LinkedList

---
+++ CPP_TLE/LinkedList/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+    class Node{
+        public:
+        int data;
+        Node* Next;
+
+        //Constructor
+        Node(int data){
+            this->data = data;
+            this -> Next = NULL;
+        }
+    };
+int main()
+{
+    Node* node1 = new Node(10);
+    cout<<node1 -> data<<endl;
+    cout<<node1 -> Next<<endl;
+    return 0;
+}
