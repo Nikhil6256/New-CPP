914af88713728cb60e78a16bf9632d46059be6fc
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2025-08-03 22:59:24 +0530

    Practice

---
+++ CPP_TLE/LinkedList/7.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data ;
+    node* next;
+
+    node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+    node* ConvertArr2LL(vector<int>&arr){
+        node* head = new node(arr[0]);
+        node* mover = head;
+        for(int i=1;i<arr.size();i++){
+            node* temp = new node(arr[i]);
+            mover -> next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    void print(node* head){
+        while(head!=NULL){
+            cout<<head->data<<" ";
+            head=head->next;
+        }
+        cout<<endl;
+    }
+
+    node* removehead(node* head){
+        if(head == NULL)
+        return head;
+        node* temp = head;
+        head=head->next;
+        delete temp;
+        return head;
+    }
+
+int main()
+{
+    vector<int>v={2,4,5,6};
+    node* head=ConvertArr2LL(v);
+    head= removehead(head);
+    print(head);
+}
