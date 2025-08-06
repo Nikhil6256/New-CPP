de6c8a546d1622dde70037163eda478de0fdbd70
Nikhil6256 <182553411+Nikhil6256@users.noreply.github.com>
2025-08-07 01:13:19 +0530

    Insertion And deletion in Linked List

--- CPP_TLE/LinkedList/7.cpp
+++ CPP_TLE/LinkedList/7.cpp
@@ -9,6 +9,10 @@ class node{
         data=data1;
         next=nullptr;
     }
+    node(int data1, node* next1){
+        data = data1;
+        next = next1;
+    }
 };
     node* ConvertArr2LL(vector<int>&arr){
         node* head = new node(arr[0]);
@@ -51,10 +55,118 @@ class node{
         return head;
     }
 
+    node* removeK(node* head, int k){
+        if(head==NULL)  return head;
+        if(k==1){
+            node* temp = head;
+            head = head->next;
+            free(temp);
+            return head;
+        }
+        int cnt=0;
+        node* temp = head;
+        node* prev = NULL;
+        while(temp!=NULL){
+            cnt++;
+            if(cnt==k){
+                prev->next = prev->next->next;
+                delete temp;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        return head;
+    }
+    node* removeEl(node* head, int el){
+        if(head==NULL)  return head;
+        if(head->data==el){
+            node* temp = head;
+            head = head->next;
+            free(temp);
+            return head;
+        }
+        node* temp = head;
+        node* prev = NULL;
+        while(temp!=NULL){
+            if(temp->data==el){
+                prev->next = prev->next->next;
+                delete temp;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        return head;
+    }
+    
+    node* InsertHead(node* head,int val){
+        node* temp = new node(val,head);
+        return temp;
+    }
+
+    node* InsertTail(node* head,int val){
+        if(head==NULL){
+            return new node(val);
+        }
+        node* temp=head;
+        while(temp->next != NULL){
+            temp=temp->next;
+        }
+        node* newNode = new node(val);
+        temp->next=newNode;
+        return head;
+    }
+
+    node* InsertPosition(node* head,int el,int k){
+        if(head==NULL){
+            if(k==1){
+                return new node(el);
+            }
+            else{
+                return head;
+            }
+        }
+        if(k==1){
+            return new node(el,head);
+        }
+        int cnt=0;
+        node* temp=head;
+        while(temp!=NULL){
+            cnt++;
+            if(cnt==(k-1)){
+                node* x= new node(el,temp->next);
+                temp->next=x;
+                break;
+            }
+            temp=temp->next;
+        }
+        return head;
+    }
+
+    node* InsertBeforeValue(node* head,int el,int val){
+        if(head==NULL){
+            return NULL;
+        }
+        if(head->data==val){
+            return new node(el,head);
+        }
+        node* temp=head;
+        while(temp->next!=NULL){
+            if(temp->next->data==val){
+                node* x=new node(el,temp->next);
+                temp->next=x;
+                break;
+            }
+            temp=temp->next;
+        }
+        return head;
+    }
+
 int main()
 {
     vector<int>v={2,4,5,6};
     node* head=ConvertArr2LL(v);
-    head= removetail(head);
+    head= InsertBeforeValue(head,100,4);
     print(head);
 }
